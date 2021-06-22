047ed78ebfe5d6a020f705aec25a825adaa23f6f
ruinwCN <ruinwcn@gmail.com>
2021-06-22 14:26:25 +0800

    add gctrance-trance

--- go/go-gctrace/example-go-gctrance-trance.go
+++ go/go-gctrace/example-go-gctrance-trance.go
@@ -2,13 +2,21 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"runtime/trace"
 	"sync"
 )
 
 func main() {
+
+	f, _ := os.Create("trace.out")
+	defer f.Close()
+	trace.Start(f)
+	defer trace.Stop()
+
 	wg := sync.WaitGroup{}
 	wg.Add(10)
-	for i := 0; i <= 10; i++ {
+	for i := 0; i < 10; i++ {
 		go func() {
 			list := make(map[int]int, 1e6)
 			for i := 0; i < 1e6; i++ {
